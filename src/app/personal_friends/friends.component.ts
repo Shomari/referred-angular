6d091a15efa455b849df9b73619c2815875cbef5
Shomari <Shomari.Ewing@gmail.com>
2017-10-01 18:46:45 -0500

    more deploy

--- src/app/personal_friends/friends.component.ts
+++ src/app/personal_friends/friends.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
-import { User } from '../interfaces/User.interface';
+import { User } from '../interfaces/user.interface';
 
 
 @Component({
